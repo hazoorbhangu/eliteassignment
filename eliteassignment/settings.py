f0186fc855941a323225ca9ed180b2a6f0386a93
komal <sharmakomal9076@gmail.com>
2024-10-13 20:53:36 +0530

    Today Code

--- eliteassignment/settings.py
+++ eliteassignment/settings.py
@@ -139,5 +139,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'eliteassignmentteam@gmail.com'
+EMAIL_HOST_USER = ' scholarwave225@gmail.com'
 EMAIL_HOST_PASSWORD = 'Komal@123'
